9fef706e9bc6f9125583e5bc57a7faa03806c89f
chravi14 <ravindra@haydenfilmsinstitute.org>
2023-10-18 21:46:40 -0500

    Added pull number

--- service-a/handler.js
+++ service-a/handler.js
@@ -8,7 +8,7 @@ export const hello = async (event, context, callback) => {
     200,
     {
       id: uid,
-      message: "11. Generated using short-unique-id pkg",
+      message: "12. Generated using short-unique-id pkg",
       bucketNames: `${process.env.serviceBS3BucketName} ${process.env.serviceCS3BucketName}`,
     },
     undefined
