10c35e091812827d0f6cdbd77048f0f3a2117497
chravi14 <ravindra@haydenfilmsinstitute.org>
2023-10-21 11:04:19 -0500

    testing imports & deploy worflow

--- service-a/handler.js
+++ service-a/handler.js
@@ -1,15 +1,35 @@
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
 import ShortUniqueId from "short-unique-id";
-import { formatResponse } from "../shared";
+import { dynamoDBDocumentClient, formatResponse } from "../shared";
 
+const usersTableName = process.env.usersTableName;
 export const hello = async (event, context, callback) => {
   const uid = new ShortUniqueId();
+
+  let user;
+  console.log("Users table name is: ", usersTableName);
+  try {
+    const params = {
+      TableName: usersTableName,
+      Key: {
+        email: "test@test.com",
+      },
+    };
+
+    const queryCommand = new GetCommand(params);
+    const result = await dynamoDBDocumentClient.send(queryCommand);
+    console.log(result.Item);
+    user = result.Item;
+  } catch (err) {
+    console.log(err);
+  }
 
   return formatResponse(
     200,
     {
       id: uid,
       message: "13. Generated using short-unique-id pkg",
-      bucketNames: `${process.env.serviceBS3BucketName} ${process.env.serviceCS3BucketName}`,
+      user: user,
     },
     undefined
   );
